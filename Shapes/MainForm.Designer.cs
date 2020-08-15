d0053163d771a97cbf6d56cc4c978c5b1a1b328e
David Warwick <bgmsd@live.com>
2020-08-15 14:00:05 -0700

    Add project files.

---
+++ Shapes/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Shapes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(12, 135);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(82, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(12, 85);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(12, 26);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Box Origin X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Box Origin Y";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(12, 216);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 10;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ Shapes/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapes
+{// Beginning of namespace
+    partial class MainForm : Form
+    {//Beginning of class
+
+        // m_myBoxes is available to the entire MainForm class
+        private List<Box> m_myBoxes;
+
+        public MainForm()
+        {
+            //Default Constructor
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //all code contained in this method 
+            //will run when the form has loaded.
+            m_myBoxes = new List<Box>();
+
+            /* m_myBoxes is now an empty
+             * usable list that can be used
+             * everywhere in the class.
+             * 
+             * If it were declared in this
+             * method, it could only be 
+             * used in this method.
+             * 
+             * For example, if we did this:
+             * List<Box> m_myBoxes; 
+             * m_myBoxes = new List<Box>();
+             * or 
+             * List<Box> m_myBoxes = new List<Box>();
+             * 
+             * then this variable could only be used inside
+             * this method
+             */
+        }
+    } // Closes MainForm class
+
+    class Box
+    {
+        private int m_iHeight; //Class Member Variables
+        private int m_iWidth;
+        private int m_iLength;
+        private int m_iOriginX;
+        private int m_iOriginY;
+
+        public Box(int iHeight, int iWidth, int iLength, int iOriginX, int iOriginY)
+        {
+            m_iHeight = iHeight; // Constructor
+            m_iWidth = iWidth;
+            m_iLength = iLength;
+            m_iOriginX = iOriginX;
+            m_iOriginY = iOriginY;
+        }
+
+        public void Draw(Graphics graphics) //Method
+        {
+            /*
+                * Obtained from 
+                * https://www.daniweb.com/programming/software-development/threads/369044/drawing-cubes-in-c-using-system-drawing
+                */
+            Point origin = new Point(m_iOriginX, m_iOriginY); //Location of Box
+            Pen pencil = new Pen(Color.Blue, 1f); //Blue pencil
+            Rectangle R = new Rectangle(origin.X, origin.Y, m_iWidth, m_iHeight);
+            graphics.DrawRectangle(pencil, R); // Draw Rectangle
+
+            graphics.DrawLine(pencil,
+                                origin.X, origin.Y,
+                                origin.X + m_iLength,
+                                origin.Y - m_iLength);// Top left edge
+
+            graphics.DrawLine(pencil,
+                                origin.X + m_iLength,
+                                origin.Y - m_iLength,
+                                origin.X + m_iWidth + m_iLength, origin.Y - m_iLength); //Top back width
+
+            graphics.DrawLine(pencil,
+                                origin.X + m_iWidth + m_iLength,
+                                origin.Y - m_iLength,
+                                origin.X + m_iWidth,
+                                origin.Y); //Top Right Edge
+
+            graphics.DrawLine(pencil,
+                                origin.X + m_iWidth + m_iLength,
+                                origin.Y - m_iLength,
+                                origin.X + m_iWidth + m_iLength,
+                                origin.Y - m_iLength + m_iHeight); //Back Right Edge
+
+            graphics.DrawLine(pencil,
+                                origin.X + m_iWidth + m_iLength,
+                                origin.Y - m_iLength + m_iHeight,
+                                origin.X + m_iWidth,
+                                origin.Y + m_iHeight); //Bottom Right Edge
+        } //Closes Draw Method
+    } //Closes Box class
+} // Closes Shapes Namespace
